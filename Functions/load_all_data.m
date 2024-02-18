7d2e42aa8b84fcc525433acd492e67bcd7991700
MariaTatuleaCodrean <160288960+MariaTatuleaCodrean@users.noreply.github.com>
2024-02-18 20:15:59 +0000

    Update load_all_data.m
    turned script into a function, to avoid clearing variables by mistake

--- Functions/load_all_data.m
+++ Functions/load_all_data.m
@@ -1,35 +1,31 @@
+function [data] = load_all_data()
+%LOAD_ALL_DATA This function reconstructs *data* structure from smaller blocks 
+% (GitHub restricts file size to 100 MB)
+%
 % NOTE: This MATLAB script was written from scratch by the members of 
 % Cambridge InfertiliTEAM for inFer GW4 network Hackathon 2024
 
-% This function reconstructs *data* structure from smaller blocks 
-% (GitHub restricts file size to 100 MB)
+% Retrieve files containing data blocks
+blockdatafiles = dir('Data/all_data_*.mat');
 
-load('Data/pop_pca.mat','modes','coeff','A_mask','B_mask')
+% Concatenate along dim = 2 of variables
+dim = 2;
 
-% Check if data exists
-if ~exist('data', 'var')
+% Initialize data variable
+data = [];
 
-    % Retrieve files containing data blocks
-    blockdatafiles = dir('Data/all_data_*.mat');
-   
-    % Concatenate along dim = 2 of variables
-    dim = 2; 
-    
-    % Initialize data variable
-    data = [];
+% Loop over data blocks
+for jj=1:length(blockdatafiles)
+    load(blockdatafiles(jj).name, 'data_block')
 
-    % Loop over data blocks
-    for jj=1:length(blockdatafiles)
-        load(blockdatafiles(jj).name, 'data_block')
-
-        % Add newly loaded block to variable
-        data = cat(dim,data,data_block);
-    end
-    
-    % Clear redundant variables
-    clear data_block
+    % Add newly loaded block to variable
+    data = cat(dim,data,data_block);
 end
+
+% Clear redundant variables
+clear data_block
 
 %% Uncomment this section if you want to check reconstruction is correct
 % % Check the code has parsed the entire data set
 % disp(size(data)) % should be 1 by 216
+end
